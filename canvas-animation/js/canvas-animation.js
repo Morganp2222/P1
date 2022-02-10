39acfb875de2b6f24d136513704efc23c720443c
mpalumbo <mpalumbo@eastsideprep.org>
2022-02-10 14:48:02 -0800

    commit

--- canvas-animation/js/canvas-animation.js
+++ canvas-animation/js/canvas-animation.js
@@ -3,6 +3,115 @@
 let myCanvas = document.getElementById("my-canvas");
 let ctx = myCanvas.getContext("2d");
 
+//get paragraph items
+let keydownOutput = document.getElementById("keydown-output");
+let keyupOutput = document.getElementById("keyup-output");
+
+//player position and movement
+let playerx = 250;
+let playery = 250;
+let playerspeed = 2;
+let playerxdir = 0;
+let playerydir = 0;
+//balls
+let ballx = 100;
+let bally = 100;
+let ballxdir = 2;
+let ballydir = 1.5;
+let ballradius = 15;
+
+
+function drawPlayer() {
+    ctx.fillRect(playerx, playery, 100, 25);
+    ctx.fillStyle = "Purple";
+}
+
+function movePlayer() {
+    playerx += (playerspeed * playerxdir);
+    playery += (playerspeed * playerydir);
+
+    //edge check
+    if (playerx < 0) {
+        playerx = 0;
+    } else if (playerx > 500 - 100) {
+        playerx = 500 - 100;
+    }
+    if (playery > 475) {
+        playery = 475
+    } else if (playery < 0) {
+        playery = 0
+    }
+}
+
+function drawBall() {
+    // draw a filled circle at ballx and bally
+    ctx.beginPath();
+    ctx.arc(ballx, bally, ballradius, 0, 2 * Math.PI);
+    ctx.fill();
+}
+
+function moveBall() {
+    bally += ballydir;
+    ballx += ballxdir;
+}
+
+function ballcollision() {
+    //vert walls
+    if ((bally > 500 - ballradius) || (bally < 0 + ballradius)) {
+        ballydir = ballydir * -1;
+    }
+    if ((ballx > 500 - ballradius) || (ballx < 0 + ballradius)) {
+        ballxdir = ballxdir * -1;
+    }
+}
+
+
+function refreshUI() {
+    ctx.clearRect(0, 0, 500, 500);
+    movePlayer();
+    drawPlayer();
+    //animate ball
+    ballcollision();
+    moveBall();
+    drawBall();
+}
+
+//when key is pressed
+
+function keyPressed(event) {
+    //get the actual key pressed
+    let key = event.keyCode;
+    keydownOutput.innerHTML = "key down code:" + key;
+    if (key === 37) {
+        playerxdir = -1;
+    } else if (key === 39) {
+        playerxdir = 1;
+    }
+    if (key === 38) {
+        playerydir = -1;
+    } else if (key === 40) {
+        playerydir = 1;
+    }
+}
+
+//when key is released 
+
+function keyReleased(event) {
+    let key = event.keyCode
+    keyupOutput.innerHTML = "key up code" + key;
+
+    if (key === 37) {
+        playerxdir = 0;
+    } else if (key === 39) {
+        playerxdir = 0;
+    }
+    if (key === 40) {
+        playerydir = 0;
+    } else if (key === 38) {
+        playerydir = 0;
+    }
+
+}
 
 // object position
 let xPosition = 0;
@@ -41,7 +150,7 @@ function moveVertical() {
 function bounceHor() {
     ctx.clearRect(0, 0, 500, 500);
     ctx.fillRect(xPosition, 0, 50, 50, );
-    ctx.fillStyle = "red";
+    ctx.fillStyle = "purple";
     xPosition = xPosition + xDirection;
     if (xPosition > 500 | xPosition < 0) {
         xDirection = -xDirection;
@@ -49,7 +158,8 @@ function bounceHor() {
 }
 
 
-//ball bouncing horizontally
 
 
-setInterval(bounceHor, 10);
+
+//setInterval(b 10);
+setInterval(refreshUI, 30);
